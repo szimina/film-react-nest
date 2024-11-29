7633b9711a96fbb6a5445c7814cacef4bfed6f3e
Svetlana Zimina <szimina@internalad.travelmart.ru>
2024-11-29 16:33:54 +0300

    for deploy #8

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,2 +1,2 @@
-export const API_URL = import.meta.env.VITE_API_URL ?? '/api/afisha';
-export const CDN_URL = import.meta.env.VITE_CDN_URL ?? '/content/afisha';
+export const API_URL = import.meta.env.VITE_API_URL ?? 'localhost/api/afisha';
+export const CDN_URL = import.meta.env.VITE_CDN_URL ?? 'localhost/content/afisha';
